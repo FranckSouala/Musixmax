5767e1db4dfae7164f97663bf808ec22556b0bbe
Souala Franck <soualafranck5@gmail.com>
2024-02-25 23:55:29 +0100

    ajout de l'icone sur l'app

--- Musixmax/Musixmax.Android/MainActivity.cs
+++ Musixmax/Musixmax.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace Musixmax.Droid
 {
-    [Activity(Label = "Musixmax", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Musixmax", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
